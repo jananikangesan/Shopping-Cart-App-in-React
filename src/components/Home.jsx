e407a1cc5abe6d9af1e461766e9fe682c60d936f
jananikangesan <janakanges1996@gmail.com>
2024-09-02 16:54:28 +0530

    product component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import data from "../assets/products.json";
 import Product from './Product';
+import '../css/Home.css'
 
 const Home = () => {
     const [products,setProducts]=useState(data);
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,7 @@ import "../css/Product.css";
 
 
 const Product = ({product}) => {
+  const name=product.name.length>21 ?product.name.substring(0,20)+"..  ":product.name;
   return (
     <div className='product'>
        <div className="img">
@@ -10,7 +11,7 @@ const Product = ({product}) => {
        </div>
 
        <div className="details">
-        <h3>{product.name}</h3>
+        <h4>{name}</h4>
         <p>Price Rs:{product.amt}</p>
         <button>Add to Cart</button>
        </div>
