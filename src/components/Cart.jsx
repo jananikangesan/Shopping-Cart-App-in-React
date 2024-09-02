c8b949e918300a14d594e2d9053969a2a9349f10
jananikangesan <janakanges1996@gmail.com>
2024-09-02 17:18:24 +0530

    view cart changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "../css/Cart.css";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <>
+        <h1 className='cart-heading'>Cart Products</h1>
+        <div className="cart-container">
+            <div className="cart-product">
+                <div className="img">
+                    <img src="" alt="image"/>
+                </div>
+            </div>
+            <div className="cart-product-details">
+                <h3>Product Name</h3>
+                <p>Price Rs: 45</p>
+            </div>
+        </div>
+        <h2 className='cart-amt'>Total Amount Rs: 135</h2>
+    </>
   )
 }
 
